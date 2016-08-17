041fe8967d2c94400d1f01666c8eda711f197326
Administrator <584532675@qq.com>
2016-08-17 16:26:42 +0800

    加图

--- app/src/main/java/qf/com/MyFragment.java
+++ app/src/main/java/qf/com/MyFragment.java
@@ -1,10 +1,24 @@
 package qf.com;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 /**
  * Created by Administrator on 2016/8/17 0017.
  */
-public class MyFragment extends Fragment {
+public class MyFragment extends AppCompatActivity{
+    ImageView iv;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        init();
+    }
 
+    private void init() {
+        iv.setImageResource(R.mipmap.ic_launcher);
+    }
 }

37c45091360b2d76a5bb6a76af96a22021af5d0e
Administrator <584532675@qq.com>
2016-08-17 16:38:54 +0800

    home分支

--- app/src/main/java/qf/com/MyFragment.java
+++ app/src/main/java/qf/com/MyFragment.java
@@ -1,31 +1,22 @@
 package qf.com;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Administrator on 2016/8/17 0017.
  */
 public class MyFragment extends AppCompatActivity {
+    ImageView iv;
+
     TextView tv;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
-        tv= (TextView) findViewById(R.id.tv);
-        tv.setText("合体成功");
-    }
-public class MyFragment extends AppCompatActivity{
-    ImageView iv;
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-        init();
-    }
-
-    private void init() {
-        iv.setImageResource(R.mipmap.ic_launcher);
     }
 }
